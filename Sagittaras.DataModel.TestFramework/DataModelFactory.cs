819a21f15e9b53b820c833a2ee789ad11feceda2
Zechy <email@zechy.cz>
2022-05-17 20:17:12 +0200

    Initial Commit.

---
+++ Sagittaras.DataModel.TestFramework/DataModelFactory.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Castle.Core.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Sagittaras.DataModel.TestFramework
+{
+    /// <summary>
+    /// Factory class for model unit tests
+    /// </summary>
+    /// <remarks>
+    /// Class is preparing testing environment and single database per every test.
+    /// </remarks>
+    public class DataModelFactory
+    {
+        /// <summary>
+        /// A name of file containing the default connection string.
+        /// </summary>
+        private const string ConnectionStringFile = "connectionString.txt";
+
+        /// <summary>
+        /// Collection for building <see cref="IServiceProvider" />
+        /// </summary>
+        private readonly ServiceCollection _serviceCollection = new();
+
+        /// <summary>
+        /// Backing field for <see cref="ServiceProvider" />
+        /// </summary>
+        private IServiceProvider? _serviceProvider;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="DataModelFactory" /> with configured services.
+        /// </summary>
+        protected DataModelFactory()
+        {
+            _serviceCollection.AddSingleton(Mock.Of<IConfiguration>());
+        }
+
+        /// <summary>
+        /// Access to Dependency Container.
+        /// </summary>
+        /// <remarks>
+        /// Provider is created on demand. If the provider has no instance yet, configuration and building process
+        /// will be called.
+        /// </remarks>
+        public IServiceProvider ServiceProvider
+        {
+            get
+            {
+                if (_serviceProvider is null)
+                {
+                    OnConfiguring(_serviceCollection);
+                    _serviceProvider = _serviceCollection.BuildServiceProvider();
+                }
+
+                return _serviceProvider;
+            }
+        }
+
+        /// <summary>
+        /// Method providing additional way to register custom services.
+        /// </summary>
+        /// <param name="services">Services collection</param>
+        protected virtual void OnConfiguring(ServiceCollection services)
+        {
+        }
+
+        /// <summary>
+        /// Loads up a default connection string from <see cref="ConnectionStringFile" /> and adds
+        /// randomly generated database name to ensure each test will be running in separate database
+        /// context.
+        /// </summary>
+        /// <returns>Default connection string.</returns>
+        /// <exception cref="DirectoryNotFoundException">Unable to determine current location.</exception>
+        protected static string GetConnectionString()
+        {
+            string? classLocation = Path.GetDirectoryName(new Uri(typeof(DataModelFactory).Assembly.Location).LocalPath);
+            if (classLocation is null)
+            {
+                throw new DirectoryNotFoundException($"Could not determine location of {nameof(DataModelFactory)}");
+            }
+
+            string filePath = Path.Combine(classLocation, ConnectionStringFile);
+            string dbName = $"xunit_{Guid.NewGuid():N}";
+            return $"{File.ReadAllLines(filePath).First()};Database={dbName}";
+        }
+    }
+}
---
+++ Sagittaras.DataModel.TestFramework/UnitTest.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Sagittaras.DataModel.TestFramework
+{
+    /// <summary>
+    /// Implementation of <see cref="IClassFixture{TFixture}" /> into custom default class for unit testing.
+    /// Class is using interface <see cref="IAsyncLifetime" />, so every Database test starts from scratch.
+    /// </summary>
+    public abstract class UnitTest<TFactory, TDbContext> : IClassFixture<TFactory>, IAsyncLifetime
+        where TFactory : DataModelFactory
+        where TDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of Unit Test with prepared helper classes.
+        /// </summary>
+        /// <remarks>
+        /// Instance of service provider for each test has custom scope.
+        /// </remarks>
+        /// <param name="factory">Factory which creates context for tests.</param>
+        /// <param name="testOutputHelper">Instance of output helper for debugging.</param>
+        protected UnitTest(TFactory factory, ITestOutputHelper testOutputHelper)
+        {
+            Factory = factory;
+            TestOutputHelper = testOutputHelper;
+            ServiceProvider = factory.ServiceProvider.CreateScope().ServiceProvider;
+            Context = ServiceProvider.GetRequiredService<TDbContext>();
+        }
+
+        /// <summary>
+        /// Instance of Factory creating Test Context.
+        /// </summary>
+        protected TFactory Factory { get; }
+
+        /// <summary>
+        /// Instance of <see cref="ITestOutputHelper" /> for output debugging purposes.
+        /// </summary>
+        protected ITestOutputHelper TestOutputHelper { get; }
+
+        /// <summary>
+        /// Instance of new Scope of <see cref="IServiceProvider" /> in current context.
+        /// </summary>
+        protected IServiceProvider ServiceProvider { get; }
+
+        /// <summary>
+        /// Instance of actual used <see cref="TDbContext" />
+        /// </summary>
+        protected TDbContext Context { get; }
+
+        /// <inheritdoc />
+        public virtual async Task InitializeAsync()
+        {
+            await Context.Database.EnsureCreatedAsync();
+        }
+
+        /// <inheritdoc />
+        public virtual async Task DisposeAsync()
+        {
+            await Context.Database.EnsureDeletedAsync();
+        }
+    }
+}
